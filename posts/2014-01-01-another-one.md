cc54f41466ed061030cbc26ebe8e1590cd47fa50
William Estoque <william.estoque@gmail.com>
2013-10-11 18:23:20 -0700

    Added working posts

---
+++ posts/2014-01-01-another-one.md
@@ -0,0 +1,5 @@
+---
+title: Another Post
+---
+
+Another Post
--- server.rb
+++ server.rb
@@ -1,4 +1,6 @@
 require "sinatra"
+require "rdiscount"
+require "yaml"
 
 set :haml, :format => :html5
 
@@ -6,6 +8,50 @@ get "/" do
   haml :index
 end
 
+get "/blog" do
+  items  = Dir["posts/*"]
+  @posts = items.map { |str| str.match(/posts\/(.*).md/)[1] }
+  haml :blog
+end
+
+get "/blog/:id" do
+  path   = File.expand_path("posts/#{params[:id]}.md")
+  text   = File.read(path)
+  parsed = parse_post(text)
+  data   = parsed[0]
+
+  # Set the posts variables
+  data.each { |k,v| instance_variable_set("@#{k}", v) }
+
+  # Set the content
+  markdown = RDiscount.new(parsed[1])
+  @content = markdown.to_html
+
+  haml :post, :layout => :post_layout
+end
+
 get "/base.css" do
   scss :base
+end
+
+# Parses the post
+#
+# content - The contents of the post
+#
+# Returns an Array
+def parse_post(content)
+  begin
+    matches     = content.match(/\A---\s*\n.*?\n?(.*)---\s*$\n?(.*)/m)
+    data_str    = matches[1]
+    content_str = matches[2]
+
+    if matches
+      data = YAML.load(data_str)
+    end
+  rescue SyntaxError => e
+    puts "YAML Exception reading content #{content}: #{e.message}"
+  rescue Exception => e
+    puts "Error reading content: #{e.message}"
+  end
+  [data || {}, content_str]
 end
